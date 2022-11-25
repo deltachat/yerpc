1decdff6721f1636e151ba14b0e396a4900e7512
link2xt <link2xt@testrun.org>
2022-11-25 17:47:22 +0000

    fixup

--- yerpc-derive/src/openrpc.rs
+++ yerpc-derive/src/openrpc.rs
@@ -15,6 +15,7 @@ fn generate_param(input: &Input, i: usize) -> TokenStream {
             name: #name.to_string(),
             description: None,
             schema: ::yerpc::openrpc::generate_schema::<#ty>(),
+            required: true
         }
 
     }
